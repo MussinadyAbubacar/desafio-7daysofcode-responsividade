3061f9ba49912aa839770f00de6b1d5482503e52
Mussinady Abubacar <mussinadyabubacar20@gmail.com>
2023-10-14 08:26:52 +0100

    terminando a resposividade

---
+++ script/script.js
@@ -0,0 +1,8 @@
+const btnMais = document.querySelector('#btn-mais')
+const display = document.querySelector('.display')
+
+btnMais.addEventListener('click', () => {
+
+    display.classList.toggle('show')
+
+})
